fda4e0b4d1e4c3f3a8ff8438a882b02f91ee095b
Ruxo Zheng <rz@just.sent.as>
2025-02-24 15:20:28 +0700

    Fix incorrect getting connection string

--- src/RZ.Foundation.Audit/Mongo/MongoAuditLogDbContext.cs
+++ src/RZ.Foundation.Audit/Mongo/MongoAuditLogDbContext.cs
@@ -6,11 +6,11 @@ using RZ.Foundation.MongoDb;
 namespace RZ.Foundation.Audit.Mongo;
 
 class MongoAuditLogDbContext(ILogger<MongoAuditLogDbContext> logger, IConfiguration configuration)
-    : RzMongoDbContext(configuration["AuditLog"] ?? NoConfig(logger))
+    : RzMongoDbContext(configuration.GetConnectionString("AuditLog") ?? NoConfig(logger))
 {
     [DoesNotReturn]
     static string NoConfig(ILogger logger) {
-        logger.LogWarning("No configuration for AuditLog");
+        logger.LogWarning("No connection string key `AuditLog`");
         Environment.Exit(-1);
         throw new Exception(); // never reached
     }
