7e80756b94d0c5805d2a747b9590b885108a3b24
Ruxo Zheng <rz@just.sent.as>
2025-06-30 16:32:10 +0700

    Allow specifying configuration key for connection string

--- src/RZ.Foundation.Audit/Mongo/MongoAuditLogDbContext.cs
+++ src/RZ.Foundation.Audit/Mongo/MongoAuditLogDbContext.cs
@@ -1,17 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using RZ.Foundation.MongoDb;
+﻿using RZ.Foundation.MongoDb;
 
 namespace RZ.Foundation.Audit.Mongo;
 
-public class MongoAuditLogDbContext(ILogger<MongoAuditLogDbContext> logger, IConfiguration configuration)
-    : RzMongoDbContext(configuration.GetConnectionString("AuditLog") ?? NoConfig(logger))
-{
-    [DoesNotReturn]
-    static string NoConfig(ILogger logger) {
-        logger.LogWarning("No connection string key `AuditLog`");
-        Environment.Exit(-1);
-        throw new Exception(); // never reached
-    }
-}
+public class MongoAuditLogDbContext(string connectionString) : RzMongoDbContext(connectionString);
--- src/RZ.Foundation.Audit/Mongo/MongoAuditLogSettings.cs
+++ src/RZ.Foundation.Audit/Mongo/MongoAuditLogSettings.cs
@@ -1,26 +1,43 @@
-﻿using JetBrains.Annotations;
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace RZ.Foundation.Audit.Mongo;
 
 [PublicAPI]
 public static class MongoAuditLogSettings
 {
-    public static IServiceCollection AddMongoAuditLog(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+    public static IServiceCollection AddMongoAuditLog(this IServiceCollection services, string configKey = "ConnectionStrings:AuditLog", ServiceLifetime lifetime = ServiceLifetime.Singleton)
         => lifetime switch {
             ServiceLifetime.Scoped => services.AddScoped<IAuditLog, MongoAuditLog>()
                                               .AddSingleton<AuditLogDispatcher>()
-                                              .AddScoped<MongoAuditLogDbContext>()
+                                              .AddScoped(CreateDbContext(configKey))
                                               .AddScoped<IAuditLogDispatcher, MongoLogDispatcher>(),
             ServiceLifetime.Singleton => services.AddSingleton<IAuditLog, MongoAuditLog>()
                                                  .AddSingleton<AuditLogDispatcher>()
-                                                 .AddSingleton<MongoAuditLogDbContext>()
+                                                 .AddSingleton(CreateDbContext(configKey))
                                                  .AddSingleton<IAuditLogDispatcher, MongoLogDispatcher>(),
             ServiceLifetime.Transient => services.AddSingleton<IAuditLog, MongoAuditLog>()
                                                  .AddSingleton<AuditLogDispatcher>()
-                                                 .AddTransient<MongoAuditLogDbContext>()
+                                                 .AddTransient(CreateDbContext(configKey))
                                                  .AddSingleton<IAuditLogDispatcher, MongoLogDispatcher>(),
 
             _ => throw new ArgumentOutOfRangeException(nameof(lifetime), lifetime, null)
         };
+
+    static Func<IServiceProvider, MongoAuditLogDbContext> CreateDbContext(string configKey) => sp => {
+        var config = sp.GetRequiredService<IConfiguration>();
+        var logger = sp.GetRequiredService<ILogger<MongoAuditLogDbContext>>();
+        var connection = config[configKey] ?? NoConfig(logger, configKey);
+        return new MongoAuditLogDbContext(connection);
+    };
+
+    [DoesNotReturn]
+    static string NoConfig(ILogger logger, string configKey) {
+        logger.LogWarning("No connection string key `{ConfigKey}` found in configuration", configKey);
+        Environment.Exit(-1);
+        throw new Exception(); // never reached
+    }
 }
